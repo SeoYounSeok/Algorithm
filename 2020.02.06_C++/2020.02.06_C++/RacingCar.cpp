c790eac93f76b1eca70af6d85ff153da44ef1b9c
SeoYounSeok <94.younseok@gmail.com>
2020-02-06 21:33:22 +0900

    #commit
    resume

--- 2020.02.06_C++/2020.02.06_C++/RacingCar.cpp
+++ 2020.02.06_C++/2020.02.06_C++/RacingCar.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define ID_LEN 20
 #define MAX_SPD 200
-#define FULL_STEP 2
+#define FUEL_STEP 2
 #define ACC_STEP 10
 #define BRK_STEP 10
 
@@ -14,12 +14,35 @@ typedef struct {
 	int curSpeed; // 속도
 }Car;
 
-void ShowCarState() {
+
+void ShowCarState(const Car &car) {
+	cout << "소유자ID: " << car.gameID << endl;
+	cout << "연료량: " << car.fuelGague << endl;
+	cout << "현재속도: " << car.curSpeed << endl;
 
 }
 
+void Accel(Car &car) {
+	if (car.fuelGague < 0)
+		return;
+	else
+		car.fuelGague -= FUEL_STEP;
+
+}
+
+void Break(Car &car) {
+	if (car.curSpeed == 0)
+		car.curSpeed = 0;
+	else
+		car.curSpeed -= FUEL_STEP;
+}
 
 int main(void) {
-	cout << "hello" << endl;
+
+	Car run99 = { "run99", 100, 0 };
+	Accel(run99);
+	Break(run99);
+	ShowCarState(run99);
+	return 0;
 
 }
