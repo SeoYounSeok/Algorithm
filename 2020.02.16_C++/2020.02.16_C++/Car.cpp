d7fc1f796629deb5b17215805e539d5c150bd3b0
SeoYounSeok <94.younseok@gmail.com>
2020-02-16 19:02:37 +0900

    ##
    @##

--- 2020.02.16_C++/2020.02.16_C++/Car.cpp
+++ 2020.02.16_C++/2020.02.16_C++/Car.cpp
@@ -8,10 +8,23 @@ private:
 	double gas;
 public:
 	Car();
-	void setCar(int num, double gas);
+	void setCar(int n, double g);
 	void show();
 
 };
+
+
+// 클래스 확장하기 
+
+class RacingCar : public Car {
+
+private:
+	int course;
+public:
+	RacingCar();
+	void setCourse(int c);
+};
+
 
 Car::Car() {
 	num = 0;
@@ -19,5 +32,35 @@ Car::Car() {
 	cout << "자동차가 만들어 졌습니다." << endl;
 }
 
+void Car::setCar(int n, double g) {
+	num = n;
+	gas = g;
+	cout << "차량 번호를" << num << "으로, 연료량을" << gas << "으로 바꾸었습니다" << endl;
 
+}
 
+void Car::show() {
+	cout << "차량 번호는" << num << "입니다" << endl;
+	cout << "연료량은 " << gas << "입니다" << endl;
+}
+
+RacingCar::RacingCar() {
+	course = 0;
+	cout << "레이싱카가 만들어졌습니다." << endl;
+
+}
+
+void RacingCar::setCourse(int c) {
+	course = c;
+	cout << "코스 번호는 " << course << " 로 정했습니다" << endl;
+
+}
+
+int main(void) {
+	RacingCar rccar1;
+
+	rccar1.setCar(1234, 20.5);
+	rccar1.setCourse(5);
+
+	return 0;
+}
