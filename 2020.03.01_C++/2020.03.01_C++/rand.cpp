1dffdbcaf819e7b7608abe3384dca853d8f927e8
SeoYounSeok <94.younseok@gmail.com>
2020-03-01 22:17:39 +0900

    #
    #로또

---
+++ 2020.03.01_C++/2020.03.01_C++/rand.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+int main(void) {
+
+	int iNum[45] = {};
+	for (int i = 0; i < 45; i++) {
+		iNum[i] = i + 1;
+	}
+	// 1부터 45까지 넣는거.. 
+
+	srand((unsigned int)time(0)); // 시간
+
+
+
+
+	return 0;
+}
