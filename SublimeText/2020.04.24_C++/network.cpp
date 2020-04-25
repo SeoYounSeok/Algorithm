0f1e36ab45ccfe8d7136a59e34d683bb0b16fe47
SeoYounSeok <syounseok94@gmail.com>
2020-04-25 21:50:40 +0900

    dfs 다시공부 필요
    4.25 이후

--- SublimeText/2020.04.24_C++/network.cpp
+++ SublimeText/2020.04.24_C++/network.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 bool dfs(vector<vector<int>> &computers, int n){
+	// 이미 순환한 노드라면 리턴(재귀함수 탈출 조건)
     if (!computers[n][n])
         return false;
+    // 순회 했다고 변경하기 
     computers[n][n] = 0;
+    // 노드수마큼 반복
     for (int i = 0; i < computers.size(); i++) {
+       // 연결된 노드가 있다면 재귀
         if (computers[n][i])
             dfs(computers, i);
     }
--- SublimeText/2020.04.24_C++/network2.cpp
+++ SublimeText/2020.04.24_C++/network2.cpp
@@ -3,32 +3,34 @@
 
 using namespace std;
 
-void DFS(int from, int n, vector<int> &visited, vector<vector<int>> &computers) {
+//bool 함수 
+int visited[200];
 
-    for (int i = 0; i < n; i++) {
-        if (from != i && computers[from][i] == 1 && visited[i] == 0) {
-            visited[i] = 1;
-            DFS(i, n, visited, computers);
-        }
+vector<vector<int>> cop;
+
+void dfs(int x)
+{
+    int i;
+    visited[x] = 1;
+
+    for(i=0;i<cop[x].size();i++)
+    {
+        if(cop[x][i] && !visited[i]) dfs(i);
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
-
-    int network = 0;
-    vector<int> visited(n, 0);
-
-    for (int i = 0; i <n; i++) {
-        if (visited[i] == 1) {
-            continue;
+    int answer = 0;
+    int i;
+    
+    cop = computers;
+    for(i=0;i<computers.size();i++)
+    {
+        if(!visited[i])
+        {
+            dfs(i);
+            answer++;
         }
-
-        // visit node and start DFS
-        network++;
-        visited[i] = 1;
-
-        DFS(i, n, visited, computers);
     }
-
-    return network;
+    return answer;
 }
